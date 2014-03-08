17c7f8242da79eeb993a05bac7353e75ad94c14f
Davis Wang <Davis@Daviss-Macbook-Pro.local>
2014-03-08 01:22:25 -0800

    Implemented enter in the text field to send message + showing the user a message when there are new messages, but only when they're not near the bottom of the chatEntries div

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,16 @@ function addMessage(msg, username) {
 function sentMessage() {
     if ($('#messageInput').val() != "") {
         socket.emit('message', $('#messageInput').val());
+
+        if (Math.abs(($('#chatEntries')[0].scrollHeight - $('#chatEntries').scrollTop() - $('#chatEntries').outerHeight()) < 20) ) {
+            $("#chatEntries").animate({
+                scrollTop: $('#chatEntries')[0].scrollHeight
+            }, 200);
+        }
+        else {
+            $('#moreMsgs').filter(':hidden').fadeIn(1000).delay(3000).fadeOut(3000);
+        }
+
         addMessage($('#messageInput').val(), "Me", new Date().toISOString(), true);
         $('#messageInput').val('');
     }
@@ -29,14 +39,18 @@ socket.on('loadUsersList', function(data) {
     // TODO gotta fix this, encode
     $('#usersList').empty();
     for (var i = 0 ; i < data.usersList.length ; i++) {
-            $('#usersList').append('<div class="username">' + data.usersList[i] + '</div>');
+        $('#usersList').append('<div class="username">' + data.usersList[i] + '</div>');
     }
 });
 
-//TODO, when a new message is received, scroll to bottom, maybe only if they are already at the bottom, if not, maybe show a prompt?
-//TODO, implement timestamp and history
-
 $(function() {
+    $('#messageInput').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $("#submit").click();
+        }
+    });
+
     $('#setUsername').click(function() {setUsername()});
     $('#submit').click(function() {sentMessage();});
 });
